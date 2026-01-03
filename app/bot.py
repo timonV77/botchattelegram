adeeb53f16005a43306686271405d126dd1ac65f
TimonV3 <163009472+TimonV3@users.noreply.github.com>
2026-01-03 04:52:28 +0200

    updatt

--- app/bot.py
+++ app/bot.py
@@ -1,20 +1,12 @@
-import aiohttp
+import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.memory import MemoryStorage
-from .config import get_settings
 
-settings = get_settings()
-
-# Специальный коннектор для борьбы с ServerDisconnectedError
-connector = aiohttp.TCPConnector(force_close=True, enable_cleanup_closed=True)
-session = AiohttpSession(connector=connector)
-
+# Просто и надежно инициализируем бота
 bot = Bot(
-    token=settings.bot_token,
-    session=session,
+    token=os.getenv("BOT_TOKEN"),
     default=DefaultBotProperties(
         parse_mode=ParseMode.HTML
     )
